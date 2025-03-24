40decf6511124d5d78a0303b02f2dd262b648420
andrewpmilne <andrew_p_milne@yahoo.co.uk>
2025-03-24 15:49:01 +0000

    add enable buttons to startGame

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -11,6 +11,17 @@ function startGame() {
     document.getElementById("goals").innerText = 0
     document.getElementById("time").innerText = 90
     document.getElementById("distance").innerText = 60
+
+    let buttons = document.getElementsByClassName('game-button');
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].disabled = false;
+    }
+
+    // Loop through the HTMLCollection to access each element
+    for (let i = 0; i < items.length; i++) {
+        console.log(items[i].innerText); // Logs the text inside each element with class 'item'
+    }
+
     let timeRemaining = parseInt(document.getElementById("time").innerText);
 
     countdown = setInterval(() => {
