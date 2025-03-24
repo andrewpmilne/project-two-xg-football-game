e4f62d6336899248c0716390981c6c7e338f0128
andrewpmilne <andrew_p_milne@yahoo.co.uk>
2025-03-24 15:35:49 +0000

    add resets to startGame function

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,16 +1,43 @@
 const startButton = document.getElementById("start-game");
 startButton.addEventListener("click", startGame);
 
+// Global Variables
+let countdown;
+let choice = [];
+
 function startGame() {
-    console.log("start");
+    clearInterval(countdown);
+    choice = [];
+    document.getElementById("goals").innerText = 0
+    document.getElementById("time").innerText = 90
+    document.getElementById("distance").innerText = 60
     let timeRemaining = parseInt(document.getElementById("time").innerText);
 
-    let countdown = setInterval(() => {
+    countdown = setInterval(() => {
         if (timeRemaining > 0) {
             timeRemaining--;
             document.getElementById("time").innerText = timeRemaining;
         } else {
             console.log("time is up")
+            clearInterval(countdown);
         }
     }, 1000);
 }
+
+function compChoice() {
+
+}
+
+function shoot() {
+
+}
+
+function forwardOne() {
+
+}
+
+function forwardTwo() {
+
+}
+
+function returnBall() {}
