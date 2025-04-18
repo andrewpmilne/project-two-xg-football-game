258d4042da8a96b72cf1cf41b20421e259226dff
andrewpmilne <andrew_p_milne@yahoo.co.uk>
2025-04-18 17:18:40 +0100

    add doc string to ensure jshint functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+/* jshint esversion: 8 */
+
 // when page loaded event listener
 document.addEventListener("DOMContentLoaded", function () {
     const welcomeModal = document.getElementById("welcome-modal");
